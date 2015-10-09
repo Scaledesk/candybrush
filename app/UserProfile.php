07cf4af46d4a704ef76886b2ca178de2f16beb51
Javed Ahamad <javed@scaledesk.com>
2015-10-09 14:56:03 +0530

    edit user profile

--- app/UserProfile.php
+++ app/UserProfile.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Collection;
 
 class UserProfile extends Model
 {
-    //
-
+    // define constants
     const TABLE = 'users_profiles';
     const USER_ID = 'candybrush_users_profiles_user_id';
     const FIRST_NAME = 'candybrush_users_profiles_first_name';
@@ -22,13 +21,12 @@ class UserProfile extends Model
     const DESCRIPTION = 'candybrush_users_profiles_description';
     const IMAGE = 'candybrush_users_profiles_image';
 
-
     protected $table = self::TABLE;
 
     protected $fillable = [self::FIRST_NAME, self::LAST_NAME, self::MOBILE, self::ADDRESS, self::STATE, self::CITY, self::PIN, self::LANGUAGE_KNOWN, self::DESCRIPTION,self::IMAGE];
 
     public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User', 'candybrush_users_profiles_user_id');
     }
 }
