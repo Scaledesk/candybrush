50988098ccf57045a4aab5ab7a008c939367c558
Javed Ahamad <javed@scaledesk.com>
2015-10-09 20:12:21 +0530

    edited user profile

--- app/libraries/Transformers/UserProfileTransformer.php
+++ app/libraries/Transformers/UserProfileTransformer.php
@@ -1,16 +1,11 @@
 <?php
-/*
-*
+/**
  * Created by PhpStorm.
  * User: Javed
  * Date: 7/10/15
  * Time: 7:23 PM
  */
-
 namespace App\libraries\Transformers;
-
-
-
 use App\UserProfile;
 use Dingo\Api\Http\Request;
 use Illuminate\Support\Facades\Input;
@@ -20,9 +15,19 @@ class UserProfileTransformer extends TransformerAbstract{
 
     public function transform(UserProfile $profile){
         return [
-            'first_name'=>$profile->candybrush_users_profiles_first_name,
-            'last_name'=>$profile->candybrush_users_profiles_last_name
-        ];
+            'name'=>$profile->candybrush_users_profiles_name,
+            'mobile'=>$profile->candybrush_users_profiles_mobile,
+            'address'=>$profile->candybrush_users_profiles_address,
+            'state'=>$profile->candybrush_users_profiles_state,
+            'city'=>$profile->candybrush_users_profiles_city,
+            'pin'=>$profile->candybrush_users_profiles_pin,
+            'language_known'=>$profile->candybrush_users_profiles_language_known,
+            'description'=>$profile->candybrush_users_profiles_description,
+            'custom_message'=>$profile->candybrush_users__profiles_custom_message,
+            'birth_date'=>$profile->candybrush_users_profiles_birth_date,
+            'sex'=>$profile->candybrush_users_profiles_sex,
+            'image'=>$profile->candybrush_users_profiles_image,
+            'id_proof'=>$profile->candybrush_users_profiles_id_proof        ];
     }
     public function requestAdapter()
     {
@@ -38,7 +43,9 @@ class UserProfileTransformer extends TransformerAbstract{
             UserProfile::SOCIAL_ACCOUNT_INTEGRATION => Input::get('social_account_integration'),
             UserProfile::CUSTOM_MESSAGE => Input::get('custom_message'),
             UserProfile::BIRTH_DATE => Input::get('birth_date'),
-            UserProfile::SEX => Input::get('sex')
+            UserProfile::SEX => Input::get('sex'),
+            UserProfile::IMAGE => Input::get('image'),
+            UserProfile::ID_PROOF => Input::get('id_proof')
         ];
     }
 }
