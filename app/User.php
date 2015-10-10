bbf39a2868542ad3767b3c81021627d0ddb0991f
Javed Ahamad <javed@scaledesk.com>
2015-10-10 11:56:25 +0530

    addded user model

---
+++ app/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Model implements AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password','confirmation_code'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /*
+     * relation with profile
+     */
+    public function userProfiles(){
+        return $this->hasOne('App\UserProfile','candybrush_users_profiles_users_id');
+    }
+    /*
+     * relation with UserWallet
+     */
+    public function userWallet(){
+        return $this->hasOne('App\UserWallet','candybrush_users_wallet_user_id');
+    }
+    /*
+     * relation with wallettransactions
+     */
+    public function transactions(){
+        return $this->hasManyThrough('UserWalletTransactions','UserWallet','candybrush_users_wallet_user_id','candybrush_users_wallet_transactions_wallet_id');
+    }
+}
