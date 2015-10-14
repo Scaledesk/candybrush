344960803219beac9c00cbb17bfdac82fec72495
Javed Ahamad <javed@scaledesk.com>
2015-10-14 12:12:00 +0530

    revert back

--- app/libraries/Transformers/MessageTransformer.php
+++ app/libraries/Transformers/MessageTransformer.php
@@ -7,17 +7,18 @@
  */
 namespace App\libraries\Transformers;
 use App\MessagesModel;
+use App\MessagesUserModel;
 use Dingo\Api\Http\Request;
 use Illuminate\Support\Facades\Input;
 use League\Fractal\TransformerAbstract;
 class MessageTransformer extends TransformerAbstract{
 
-    public function transform(MessagesModel $message){
+    public function transform(MessagesUserModel $message){
         return [
             'subject'=>$message->candybrush_messages_subject,
             'body'=>$message->candybrush_messages_body,
-            'status'=>$message->candybrush_messages_status ,
-            'user'=>self::includeUser($message)];
+            'status'=>$message->candybrush_messages_status
+            /*,'user'=>self::includeUser($message)*/];
     }
     public function requestAdapter()
     {
