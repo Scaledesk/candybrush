5a7fabf359c3ac7b97cbba52726ced710e9a4477
scaledesk_tushar <tushar@scaledesk.com>
2015-11-02 20:32:47 +0530

    add google login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -332,6 +332,8 @@ $api->post('auth/login', function (\Illuminate\Http\Request $request){
             die;
             echo $swagger->getResource($swagger->getResourceList(),array('output' => 'json'));
         });
+        //for google login
+        $api->post('auth/google','App\Http\Controllers\Auth\AuthController@google');
     });
 
 /*
