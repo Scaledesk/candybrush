847b667d076ce2482dcd84689412ec9d9895c742
scaledesk_tushar <tushar@scaledesk.com>
2015-11-05 15:34:39 +0530

    add photos to packages in bulk

--- app/Http/Controllers/PackagePhotoController.php
+++ app/Http/Controllers/PackagePhotoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use app\libraries\Transformers\PackagePhotoTransformer;
+use App\PackagePhoto;
+use App\PackagesModel;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
-class PackagePhotoController extends Controller
+class PackagePhotoController extends BaseController
 {
+    protected $package_photo_transformer;
+
+    function __construct()
+    {
+        $this->package_photo_transformer=new PackagePhotoTransformer();
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -29,15 +40,83 @@ class PackagePhotoController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    /*public function store(Request $request)
     {
-        //
+        $data=$this->package_photo_transformer->requestAdaptor();
+        $validation_result=$this->my_validate([
+            'data'=>$data,
+            'rules'=>[
+            PackagePhoto::URL=>'required',
+                PackagePhoto::PACKAGE_ID=>'required|numeric|exists:candybrush_packages,id',
+            ],
+            'messages'=>[
+                PackagePhoto::URL.'.required'=>'photo url is required try url=<url of the photo>',
+                PackagePhoto::PACKAGE_ID.'.required'=>'package id is required try package_id=<package_id of the photo>',
+                PackagePhoto::PACKAGE_ID.'.numeric'=>'only numbers are allowed as package id',
+                PackagePhoto::PACKAGE_ID.'.exists'=>'package id do not match any records',
+            ]
+        ]);
+        if($validation_result['result']){
+            PackagePhoto::create([$data]);
+            return $this->success();
+        }else{
+            return
+            $validation_result['error'];
+        }
+    }*/
+    /**
+     * function to store photos in bulk for a package
+     * @return mixed
+     */
+    public function store(){
+        $data=$this->package_photo_transformer->requestAdaptor();
+        if(is_null($data)){
+            return $this->error('please give data in array like "package_id":"package_id",
+            data:[
+            ["url":"url of package_photo1",
+            ], [package_photo2], [package_photo3] etc]',422);
+        }
+        $package_id=$data['package_id'];
+        unset($data['package_id']);
+        $package=PackagesModel::where('id',$package_id)->first();
+        if(is_null($package)){
+            unset($package);
+            return $this->error('Package_id do not match any records!',404);
+        }
+        unset($package);
+        foreach($data as $package_photo){
+            $validation_result=$this->my_validate([
+                'data'=>$data,
+                'rules'=>[
+                    PackagePhoto::URL=>'required',
+                ],
+                'messages'=>[
+                    PackagePhoto::URL.'.required'=>'photo url is required try url=<url of the photo>',
+                ]
+            ]);
+            if(!$validation_result['result']){
+                return $validation_result['error'];
+            }
+        }
+        $package_photo_bulk=array();
+        foreach($data as $package_photo){
+                $package_photo[PackagePhoto::PACKAGE_ID]=$package_id;
+                array_push($package_photo_bulk,$package_photo);
+
+        }
+        $result=DB::transaction(function()use($package_photo_bulk){
+            try{
+                DB::table('candybrush_packages_photos')->insert($package_photo_bulk);
+                return $this->success();
+            }catch(Exception $e){
+                return $this->error('unknown error occurred',520);
+            }
+        });
+        unset($package_id);
+        unset($package_photo_bulk);
+        unset($package_photo);
+        return $result;
+
     }
 
     /**
--- app/libraries/Transformers/PackagePhotoTransformer.php
+++ app/libraries/Transformers/PackagePhotoTransformer.php
@@ -14,15 +14,56 @@ use Illuminate\Support\Facades\Input;
 use League\Fractal\TransformerAbstract;
 
 class PackagePhotoTransformer extends TransformerAbstract{
-    public function transform(){
+    protected $availableIncludes=['Package'];
+    public function transform(PackagePhoto $packagePhoto){
         return[
-            
+            'id'=>$packagePhoto[PackagePhoto::ID],
+            'url'=>$packagePhoto[PackagePhoto::URL],
         ];
     }
-    public function requestAdaptor(PackagePhoto $packagePhoto){
-    return[
-        PackagePhoto::URL=>Input::get('url',''),
-        PackagePhoto::PACKAGE_ID=>Input::get('package_id',''),
-        ];
+    /*    public function requestAdaptor(){
+        return[
+            PackagePhoto::URL=>Input::get('url',''),
+            PackagePhoto::PACKAGE_ID=>Input::get('package_id',''),
+            ];
+        }*/
+    /**
+     * function to take in put from user in array
+     * @return array|null
+     */
+    public function requestAdaptor(){
+        $data=array();
+        $process_data=Input::get('data','');
+        $package_id=Input::get('package_id',NULL);
+        if(is_null($package_id)){
+            unset($package_id);
+            unset($process_data);
+            return NULL;
+        }
+        if($process_data&&is_array($process_data)){
+            foreach ($process_data as $package_photos) {
+                array_push($data, [
+                    PackagePhoto::URL=>Input::get('url',''),
+                ]);
+            }
+            unset($process_data);
+            $data['package_id']=$package_id;
+            unset($package_id);
+            return $data;
+        }
+        else{
+            unset($process_data);
+            unset($package_id);
+            return NULL;
+        }
+    }
+
+    /**
+     * function to include package
+     * @param PackagePhoto $packagePhoto
+     * @return \League\Fractal\Resource\Item
+     */
+    public function includePackage(PackagePhoto $packagePhoto){
+        return $this->item($packagePhoto->package(),new \App\libraries\Transformers\PackagesTransformer());
     }
 }
